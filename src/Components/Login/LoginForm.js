3f3e2af911a6a108e0f64028ec4c712453f8a40a
Tatiana Viana <tatisabinoviana@gmail.com>
2022-07-12 21:00:09 -0300

    Criação do useform para validacao do campo de login, contendo o regex da validação do email. Feito também o refinamento da busca do login e senha no loginForm.

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm('email');
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -14,7 +15,7 @@ const LoginForm = () => {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ username, password })
+      body: JSON.stringify()
     })
       .then(response => {
         console.log(response);
@@ -28,8 +29,8 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
