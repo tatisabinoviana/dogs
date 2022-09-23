33001ef03d35f308748351a95cb3813abdef2820
Tatiana Viana <tatisabinoviana@gmail.com>
2022-09-22 21:14:34 -0300

    criando a rota conta e protegendo ela caso o usuário não esteja logado

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import ProtectedRouter from './Components/Helper/ProtectedRoute';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/User';
 import { UserStorage } from './UserContext';
 
 function App() {
@@ -16,6 +18,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <Route
+              path="/conta/*"
+              element={
+                <ProtectedRouter>
+                  <User />
+                </ProtectedRouter>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
