25b45024fd072cf87a6d9e844fb9eb5cfe07b8d9
Tatiana Viana <tatisabinoviana@gmail.com>
2022-10-29 22:25:12 -0300

    Começando a trabalhar no form de postagem das fortos. Criando o arquivo css e trabalhando na lógica inicial do form.

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import styles from './UserPhotoPost.module.css';
 
 const UserPhotoPost = () => {
-  return <div>UserPhotoPost</div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  function handleImgChange() {}
+  return (
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+      </form>
+    </section>
+  );
 };
 
 export default UserPhotoPost;
