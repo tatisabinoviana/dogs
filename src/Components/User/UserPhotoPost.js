074b97726d9b7a7bda20e590f8bd9a5a9084e830
Tatiana Viana <tatisabinoviana@gmail.com>
2022-10-31 22:09:14 -0300

    construindo a interação e validação dos inputs criados

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,20 +1,45 @@
 import React from 'react';
+import { PHOTO_POST } from '../../Api';
+import useFetch from '../../Hooks/useFetch';
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 import styles from './UserPhotoPost.module.css';
 
 const UserPhotoPost = () => {
+  const nome = useForm();
+  const peso = useForm('number');
+  const idade = useForm('number');
+  const [img, setImg] = React.useState({});
+  const { data, error, loading, request } = useFetch();
+
   function handleSubmit(event) {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append('img', img.raw);
+    formData.append('nome', nome.value);
+    formData.append('peso', peso.value);
+    formData.append('idade', idade.value);
+
+    const token = window.localStorage.getItem('token');
+    const { url, options } = PHOTO_POST(formData, token);
+
+    request(url, options);
   }
-  function handleImgChange() {}
+  function handleImgChange({ target }) {
+    setImg({
+      raw: target.files[0]
+    });
+  }
   return (
     <section className={`${styles.photoPost} animeLeft`}>
       <form onSubmit={handleSubmit}>
-        <Input label="Nome" type="text" name="nome" />
-        <Input label="Peso" type="text" name="peso" />
-        <Input label="Idade" type="text" name="idade" />
+        <Input label="Nome" type="text" name="nome" {...nome} />
+        <Input label="Peso" type="number" name="peso" {...peso} />
+        <Input label="Idade" type="number" name="idade" {...idade} />
         <input type="file" name="img" id="img" onChange={handleImgChange} />
       </form>
+      <Button>Enviar</Button>
     </section>
   );
 };
